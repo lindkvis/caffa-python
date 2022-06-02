a9648f35801f0d26604999589b8387aac8178775
Gaute Lindkvist <glindkvist@kontur.tech>
2022-06-02 09:07:37 +0200

    Change back to root folder at the end of code generation

--- __init__.py
+++ __init__.py
@@ -9,20 +9,22 @@ from grpc_tools import protoc
 
 name = "caffa"
 
-
-current_dir = os.path.dirname(__file__)
+current_dir = os.path.abspath(os.curdir)
+print (current_dir)
+caffa_dir = os.path.dirname(__file__)
 generated_dir = "generated/"
 proto_dir = "protos/"
 
-sys.path.insert(0, os.path.join(current_dir, generated_dir))
+sys.path.insert(0, os.path.join(caffa_dir, generated_dir))
 
 
 def generate_proto_code():
-    os.chdir(current_dir)
+    os.chdir(caffa_dir)
     if not os.path.exists(generated_dir):
         print("Making directory " + generated_dir)
         os.mkdir(generated_dir)
     os.system("./generate_grpc_classes.sh")
+    os.chdir(current_dir)
 
 
 generate_proto_code()
