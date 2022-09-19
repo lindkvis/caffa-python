c56e84240bb42d69b2c6493e4833bfc724db5641
Gaute Lindkvist <glindkvist@kontur.tech>
2022-09-19 14:28:13 +0200

    Remove Generate GRPC classes message

--- generate_grpc_classes.bat
+++ generate_grpc_classes.bat
@@ -1,4 +1,3 @@
-echo "Generating Python GRPC classes"
 python -m grpc_tools.protoc -Iprotos --python_out=generated/ --grpc_python_out=generated/ protos/App.proto
 python -m grpc_tools.protoc -Iprotos --python_out=generated/ --grpc_python_out=generated/ protos/FieldService.proto
 python -m grpc_tools.protoc -Iprotos --python_out=generated/ --grpc_python_out=generated/ protos/ObjectService.proto
--- generate_grpc_classes.sh
+++ generate_grpc_classes.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-echo "Generating Python GRPC classes"
 python -m grpc_tools.protoc -Iprotos --python_out=generated/ --grpc_python_out=generated/ protos/*.proto
