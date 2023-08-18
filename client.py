23e50eeaeb2b64ebea2814d47fcfaa8e2e89f379
Gaute Lindkvist <glindkvist@3d-radar.com>
2023-08-18 14:26:02 +0200

    v1.0.0

--- client.py
+++ client.py
@@ -31,8 +31,8 @@ from types import SimpleNamespace
 # By default we try to match the caffa-version
 # However the application using caffa should set its own version which can be checked
 # against by providing the script-version parameter
-MIN_APP_VERSION = (0, 99, 0)
-MAX_APP_VERSION = (0, 99, 99)
+MIN_APP_VERSION = (1, 0, 0)
+MAX_APP_VERSION = (1, 0, 99)
 
 class SessionType(IntEnum):
     INVALID   = 0
